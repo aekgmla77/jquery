f3b68e706a3a50c0eb4491614d50b260c1fed9cc
aekgmla77 <aekgmla@naver.com>
2021-01-11 23:27:04 +0900

    ss

---
+++ js/showPage1.js
@@ -0,0 +1,96 @@
+/**
+ * showPage.js
+ */
+
+function showPage() {
+	let doc = xhtp.responseXML;
+	let data = doc.getElementsByTagName('record');
+	console.log(data);
+	let tableTag = document.createElement('table');
+	tableTag.setAttribute('border', '1');
+	tableTag.setAttribute('id', 'tbl');
+
+	//title tr
+	//data tr => [배열]
+	let headerTr = titleRow(data);
+	let dataTrs = contentRow(data);
+	tableTag.appendChild(headerTr);
+	for (let i = 0; i < dataTrs.length; i++) {
+		tableTag.appendChild(dataTrs[i]);
+	}
+	document.getElementById('show').appendChild(tableTag);
+}
+
+function titleRow(result) {
+	let trTag = document.createElement('tr');
+	for (let i = 0; i < result[0].childNodes.length; i++) {
+		let thTag = document.createElement('th');
+		thTag.onmouseover = function() {
+			this.style.background = 'lightgreen';
+		}
+		thTag.onmouseout = function() {
+			this.style.background = 'white';
+		}
+		let textNode = document.createTextNode(result[0].childNodes[i].nodeName.toUpperCase());
+		thTag.appendChild(textNode);
+		trTag.appendChild(thTag);
+	}
+	let thTag = document.createElement('th');
+	thTag.onmouseover = function() {
+		this.style.background = 'lightgreen';
+	}
+	thTag.onmouseout = function() {
+		this.style.background = 'white';
+	}
+	let textNode = document.createTextNode('Delete');
+	thTag.appendChild(textNode);
+	trTag.appendChild(thTag);
+
+	return trTag;
+}
+
+function contentRow(result) {
+	let trTags = [];
+	for (let j = 0; j < result.length; j++) {
+		let trTag = document.createElement('tr');
+		trTag.onclick = function() {
+		}
+		for (let i = 0; i < result[0].childNodes.length; i++) {
+			let tdTag = document.createElement('td');
+			tdTag.onmouseover = function() {
+				this.style.background = 'lightblue';
+			}
+			tdTag.onmouseout = function() {
+				this.style.background = 'white';
+			}
+			let textNode = document.createTextNode(result[j].childNodes[i].firstChild.nodeValue);
+			tdTag.appendChild(textNode);
+			trTag.appendChild(tdTag);
+		}
+		//임의로 버튼 추가.
+		let button = document.createElement('button');
+		button.innerHTML = 'Del';
+		button.onclick = function() {
+			console.log(this.parentNode.parentNode.remove());
+			let id = this.parentNode.parentNode.childNodes[0].firstChild.nodeValue;
+			let req = new XMLHttpRequest();
+			req.open('get', '../deleteEmp?empId=' + id);
+			req.send();
+			req.onload = function() {
+				console.log(req.responseText);
+			}
+		}
+		let tdTag = document.createElement('td');
+		tdTag.onmouseover = function() {
+			this.style.background = 'lightblue';
+		}
+		tdTag.onmouseout = function() {
+			this.style.background = 'white';
+		}
+		tdTag.appendChild(button);
+		trTag.appendChild(tdTag);
+
+		trTags.push(trTag);
+	}
+	return trTags;
+}
